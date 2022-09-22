1f9ec84fc60e4be0c45a87380fdd1cc20244b549
Clarence <47097322+clarence1979@users.noreply.github.com>
2022-09-22 22:48:10 +1000

    Add files via upload

---
+++ rpi_start_comp.sh
@@ -0,0 +1,26 @@
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install idle
+sudo apt-get install minecraft-pi
+sudo apt-get install python
+sudo apt-get install python3
+sudo apt-get install pip
+sudo apt-get install pip3
+cd Desktop
+sudo git clone https://github.com/DexterInd/Raspbian_For_Robots.git
+cd Raspbian_For_Robots
+sudo bash update_master.sh
+cd ..
+sudo git clone https://github.com/bennuttall/sense-hat-examples.git
+sudo apt-get install gedit
+
+pip3 install --upgrade numpy
+pip3 install djitellopy
+
+pip3 install python-espeak
+pip install python-espeak
+pip3 install python-easygui
+pip install python-easygui
+
+
